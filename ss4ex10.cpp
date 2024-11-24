75dd89dfb1b46e139bf0918f0becac59a84c06e1
mthang24242424 <wunkevan@gmail.com>
2024-11-24 19:22:27 +0700

    ss4ex10

--- ss4ex10.cpp
+++ ss4ex10.cpp
@@ -1,5 +1,5 @@
 #include<stdio.h> 
-int main(){
+int main(){ 
 	int firstNum,secondNum,thirdNum,i;
 	printf("nhap so thu nhat ");
 	scanf("%d",&firstNum);
@@ -7,21 +7,25 @@ int main(){
 	scanf("%d",&secondNum);
 	printf("nhap so thu ba ");
 	scanf("%d",&thirdNum);
+	// ktra neu so thu nhat > so thu 2 thi doi vi tri cho nhau qua i 
 	if (firstNum>secondNum){
 		i = firstNum; 
 		firstNum = secondNum; 
 		secondNum = i; 
 	} 
+	// ktra neu so thu nhat > so thu 3 thi doi vi tri cho nhau qua i 
 	if (firstNum>thirdNum){
 		i = firstNum;
 		firstNum =  thirdNum;
 		thirdNum = i; 
 	} 
+	// ktra neu so thu 2 > so thu 3 thi doi vi tri cho nhau qua i 
 	if (secondNum>thirdNum){
 		i = secondNum;
 		secondNum =  thirdNum;
 		thirdNum = i; 
 	} 
+	// in kqa theo thu tu so t1, t2, t3 sau khi da xep xong 
 	printf("%d %d %d",firstNum,secondNum,thirdNum); 
 	return 0; 
 } 
